a515ee0cb5022014f54e99482213b17174b0ae44
dortkthf <dortkthf@gmail.com>
2023-10-14 12:06:30 +0900

    modify

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dortkthf:DO45tl45@cluster0.5w8vjd8.mongodb.net/', {
+mongoose.connect('mongodb+srv://@cluster0.5w8vjd8.mongodb.net/', {
     useNewUrlParser: true, useUnifiedTopology:true,
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
