a6a2234ad98cdbcdd0df3574b512fe615471d7cb
dortkthf <dortkthf@gmail.com>
2023-10-16 17:04:35 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -5,15 +5,42 @@ dotenv.config()
 
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
+const config = require('./config/key');
+
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://${process.env.MongooseAdmin}@cluster0.5w8vjd8.mongodb.net/`, {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true,
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', (req,res) => res.send('Hello World! '))
+
+app.post('/register', (req,res) => {
+
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  user.save()
+  .then(() => {
+    return res.status(200).json({
+      success:true
+    })
+  })
+  .catch((err) => {
+    return res.json({ success:false, err })
+  })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
-const mongoose = require(mongoose);
-
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -34,4 +33,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = {User}
+module.exports = { User }
