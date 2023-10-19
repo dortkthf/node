ba1777be67e55c69389cecafaa0c30ec296c6dfa
dortkthf <dortkthf@gmail.com>
2023-10-20 00:06:13 +0900

    hoc auth + final boiler plate

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
-import {Button} from 'antd'
+import {useNavigate} from 'react-router-dom' 
+import Auth from '../../../hoc/auth'
+
 
 function LandingPage() {
 
@@ -13,11 +15,31 @@ function LandingPage() {
         console.error('에러 내용', error)
     })
   }, [])
+  const navigate = useNavigate()
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`)
+    .then(response => {
+      console.log(response.data)
+      if(response.data.success) {
+        navigate('/login')
+        alert('로그아웃이 완료되었습니다.')
+      } else {
+        alert('로그아웃 하는데 실패 했습니다.')
+      }
+    })
+  }
 
   return (
-    <Button type='primary'>LandingPage 랜딩페이지</Button>
-  )
+      <div style={{
+        display: 'flex', justifyContent: 'center', alignItems:'center', width:'100%', height:'100vh'
+      }}>
+        <h2>시작 페이지</h2>
+
+        <button onClick={onClickHandler}>로그아웃</button>
+      </div>
+    )
 }
 
-export default LandingPage
+export default Auth(LandingPage, null);
 
